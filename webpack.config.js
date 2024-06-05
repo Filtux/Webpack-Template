7d4cf5bb6654ca295ade03805d8cff4711966d9a
Joe Matthews <joe@joematthews.com.au>
2024-06-05 12:13:25 +0930

    Webpack-Template Push

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: false,
+    }),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
